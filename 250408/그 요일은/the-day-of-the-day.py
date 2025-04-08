522f99bb37e371cfdc34682dc4c37c91aa75eb1b
허재훈 <108162901+derekHeo@users.noreply.github.com>
2025-04-08 23:30:24 +0900

    Title: 그 요일은, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250408/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,20 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+
+Dow = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+
+month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+x = sum(month[:m1]) + d1
+y = sum(month[:m2]) + d2
+
+idx = Dow.index(A)
+
+if idx == 1:
+    print(((y - x) // 7))
+elif idx == 0:
+    print(((y - x) // 7))
+elif idx > (y-x)%7:
+    print(((y-x) // 7) + 1)
