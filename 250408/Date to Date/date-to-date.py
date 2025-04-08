bc4e7a8c55e4fe1a8588ec06dd1c63b3321dfa58
허재훈 <108162901+derekHeo@users.noreply.github.com>
2025-04-08 20:21:47 +0900

    Title: Date to Date, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250408/Date to Date/date-to-date.py
+++ 250408/Date to Date/date-to-date.py
@@ -2,6 +2,6 @@ m1, d1, m2, d2 = map(int, input().split())
 
 # Please write your code here.
 
-m_list = [31,28,31,30,31,30,31,30,31,30,31,30]
+m_list = [0,31,28,31,30,31,30,31,30,31,30,31]
 
-print((sum(m_list[:m2-1]) + d2) - (sum(m_list[:m1-1])+ d1))
+print((sum(m_list[:m2]) + d2) - (sum(m_list[:m1]) + d1))
