562f64f9798799776ebc76c71b491151a7f20665
허재훈 <108162901+derekHeo@users.noreply.github.com>
2025-04-08 20:46:45 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250408/DateTime to DateTime/datetime-to-datetime.py
+++ 250408/DateTime to DateTime/datetime-to-datetime.py
@@ -6,10 +6,10 @@ day_gap = a - 11
 
 if day_gap == 0:
     gap = (b * 60 + c) - (11 * 60 + 11)
-    if gap < 0:
+    if gap <= 0:
         print(-1)
     else:
         print(gap)
 
 else:
-    print((b * 60 + c) - (11 * 60 + 11) + (60 * 24))
+    print((b * 60 + c) - (11 * 60 + 11) + (day_gap * 60 * 24))
